fea6a543e505337e16c65642eaa03ce031776a95
daniel <daniel.lopez@escotel.com.mx>
2025-01-14 11:54:17 -0600

    se agrega context para responder mensajes

--- cons/typeMessages.js
+++ cons/typeMessages.js
@@ -1,7 +1,8 @@
 const typeMessages = {
   audio: 'Mensaje de voz recibido',
   document: 'Documento Recibido',
-  video:'Video recibido'
+  video:'Video recibido',
+  image:'Imagen recibido'
 }
 
 module.exports = {
